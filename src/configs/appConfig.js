ac4b728224ce636a5fde71f43b1757dac931e8f5
Knarfux <franck.hild@gmail.com>
2019-07-19 16:09:45 +0200

    Add logger

---
+++ src/configs/appConfig.js
@@ -0,0 +1,4 @@
+export const appConfig = {
+	name: process.env.APP_NAME || 'express-api-boilerplate',
+	version: process.env.APP_VERSION || process.env.NODE_ENV || "development"
+}
---
+++ src/logger.js
@@ -0,0 +1,62 @@
+import winston from 'winston'
+
+import { appConfig } from './configs'
+
+const {
+	combine,
+	timestamp,
+	prettyPrint,
+	printf,
+	align,
+	colorize,
+	json
+} = winston.format
+
+const customLevels = {
+	levels: { crit: 0, error: 1, warning: 2, info: 3, debug: 4 },
+	colors: {
+		crit: 'red',
+		error: 'red',
+		warning: 'yellow',
+		info: 'white',
+		debug:'grey'
+	}
+}
+
+const timeFormat = 'DD-MM-YYYY HH:mm:ss'
+
+const format = process.env.NODE_ENV === 'production'
+	? combine(timestamp({ format: timeFormat }), json())
+	: combine(
+		winston.format(info => {
+			info.level = info.level.toUpperCase()
+			return info
+		})(),
+		align(),
+		timestamp({ format: timeFormat }),
+		colorize(),
+		printf(({ level, message, label, timestamp, meta }) => {
+			const labelDisplay = label ? `[${label}]` : ""
+			const infoDisplay = `[${timestamp}][${level}]${labelDisplay}`
+			const metaDisplay = meta ? `\n${JSON.stringify(meta, null, 2)}` : ''
+			return `${appConfig.name}.${appConfig.version} ${infoDisplay} ${message}${metaDisplay}`
+		}),
+	)
+
+const logger = winston.createLogger({
+	levels: customLevels.levels,
+	level: process.env.LOG_LEVEL || 'info',
+	transports: [new winston.transports.Console({ format })],
+	format: combine(
+		timestamp(),
+		prettyPrint()
+	)
+})
+
+winston.addColors(customLevels.colors)
+
+logger.stream = {
+	write: message => logger.info(message, { label: 'express' })
+}
+
+export default logger
