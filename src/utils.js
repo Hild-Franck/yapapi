53dd84af5bea2a6d4d097acf648982d6f2072935
Franck Hild <franck.hild@wapitea.io>
2020-05-04 22:27:22 -0400

    Add REST routes util

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
+import { forEach } from 'lodash'
+
+const REST = {
+	create: 'post',
+	update: 'post',
+	delete: 'delete',
+	get: 'get',
+	getAll: 'get'
+}
+
+const getPath = controllerName => controllerName == 'get' ? '/' : '/:id'
+
 // Wrapper used to catch any error throwed by a controller
 export const wrap = fn => (...args) => fn(...args).catch(args[2])
 
@@ -5,3 +17,6 @@ export const sendResponse = (res, message, data, statusCode=200) => {
 	res.status(statusCode)
 	res.json({ message, data, statusCode })
 }
+
+export const createRestRoutes = (router, controller) =>
+	(forEach(REST, (k, v) => router[v](getPath(k), controller[k])), router)
