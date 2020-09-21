492e1fe419c6bdd3f56b747f7f3192692750ed94
Knarfux <franck.hild@gmail.com>
2020-09-20 20:56:01 -0400

    Add habits routes, controller, models and unit tests

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,8 @@ import health from './health'
 import auth from './auth'
 import mood from './mood'
 import note from './note'
+import habit from './habit'
 
-const controllers = { health, auth, mood, note }
+const controllers = { health, auth, mood, note, habit }
 
 export default controllers
