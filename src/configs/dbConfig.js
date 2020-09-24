79e891ca39bf42375d78fe7dfb709776fda6e761
Knarfux <knarfux@gmail.com>
2020-09-24 04:51:17 -0400

    Add more option to mongo uri

--- src/configs/dbConfig.js
+++ src/configs/dbConfig.js
@@ -2,6 +2,7 @@ export const dbConfig = {
 	host: process.env.MONGO_HOST || 'localhost',
 	username: process.env.MONGO_USERNAME,
 	password: process.env.MONGO_PASSWORD,
+	queryString: process.env.MOGO_QUERYSTRING || "",
 	database: 'db',
 	currentMigration: '',
 	pepper: process.env.PEPPER || '',
